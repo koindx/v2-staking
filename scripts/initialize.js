2137af768658298ba279203b8c69c35274f1b996
eder lobo <ederandreslobo1998@gmail.com>
2024-03-15 17:22:14 -0500

    add logs and script in initialize

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,5 +1,5 @@
 const { Signer, Provider, Contract, utils } = require('koilib');
-const abi = require('./staking-abi.json')
+const abi = require('./../abi/staking-abi.json')
 require('dotenv').config();
 
 
@@ -14,14 +14,21 @@ async function main() {
   const signer = Signer.fromWif(PRIVATE_KEY);
   signer.provider = provider;
 
-  const bridgeContract = new Contract({
+  const newContract = new Contract({
     id: signer.address,
     abi,
     provider,
     signer,
   });
 
-  const result = await bridgeContract.functions.initialize({
+  console.log({
+    tokenDeposit: TOKEN_DEPOSIT,
+    tokenReward: TOKEN_REWARD,
+    blockReward: BLOCK_REWARD,
+    blockStart: BLOCK_START,
+    blockEnd: BLOCK_END
+  })
+  const result = await newContract.functions.initialize({
     tokenDeposit: TOKEN_DEPOSIT,
     tokenReward: TOKEN_REWARD,
     blockReward: BLOCK_REWARD,
@@ -30,6 +37,7 @@ async function main() {
   });
 
   await result.transaction.wait();
+  console.log(result)
   console.log('initialize staking');
 }
 
